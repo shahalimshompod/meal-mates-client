2f72de951b0f5c82dd901104ed157fdd351f44a2
shahalimshompod <shahalimsompod@gmail.com>
2025-02-12 19:58:38 +0600

    added error page and all other things needed

--- src/Components/Error/ErrorPage.jsx
+++ src/Components/Error/ErrorPage.jsx
@@ -1,37 +1,48 @@
 import { Link, useLocation, useRouteError } from "react-router-dom";
-import error from "../../assets/logo/404.png"
 import { useEffect } from "react";
+import errorImage from "../../assets/logo/404.png";
+
 const ErrorPage = () => {
-    useEffect(() => {
-        document.title = "404 PAGE NOT FOUND";
-    }, []);
-    const errorType = useRouteError()
+  useEffect(() => {
+    document.title = "404 - Page Not Found";
+  }, []);
 
-    const Location = useLocation()
-    const path = Location.pathname;
+  const error = useRouteError();
+  const location = useLocation();
+  const path = location.pathname;
 
-    return (
-        <section className="w-screen h-screen pt-12 px-3 xl:px-0 bg-gradient-to-br from-btnHover/50 via-button to-btnHover/50">
-            <div className="flex flex-col items-center gap-6">
-                <h1 className="md:text-[280px] text-7xl font-bold text-center flex items-center ">4 <span><img className="md:w-[240px] w-16" src={error} alt="error" /></span> 4</h1>
+  return (
+    <section className="flex flex-col items-center justify-center w-full h-screen bg-gradient-to-br from-gray-200 via-gray-300 to-gray-400 px-4 text-center">
+      <h1 className="text-7xl md:text-9xl font-bold flex items-center">
+        4{" "}
+        <span>
+          <img className="w-16 md:w-32" src={errorImage} alt="404 Error" />
+        </span>{" "}
+        4
+      </h1>
 
-                <div className="flex items-center gap-2">
-                    <h3 className="text-3xl font-semibold font-montserrat">Oops!!!</h3>
-                    <p className="font-montserrat">Page {errorType.statusText || errorType.message}</p>
-                </div>
+      <h3 className="text-2xl md:text-3xl font-semibold mt-4">
+        Oops! Page Not Found
+      </h3>
+      <p className="text-lg text-gray-700 mt-2">
+        {error?.statusText || error?.message}
+      </p>
 
-                <div className="flex flex-col md:flex-row items-center gap-2 font-montserrat">
-                    <p>Estimated Root Cause:</p>
-                    <p className="border-2 bg-accent rounded-full px-2">{path}</p>
-                    <p>is not a valid path.</p>
-                </div>
+      <div className="flex flex-wrap justify-center items-center gap-2 mt-4 text-lg text-gray-800">
+        <p>Possible Cause:</p>
+        <span className="border px-3 py-1 rounded-full bg-black/20 text-white">
+          {path}
+        </span>
+        <p>is not a valid route.</p>
+      </div>
 
-                <Link to='/'>
-                    <button className="btn rounded-xl bg-orange border-none text-white hover:bg-orange text-base font-bold  hover:text-black ">Go Home</button>
-                </Link>
-            </div>
-        </section>
-    );
+      <Link to="/" className="mt-6">
+        <button className="px-6 py-3 text-lg font-semibold bg-orange text-white rounded-lg hover:bg-blue-700 transition duration-300">
+          Return Home
+        </button>
+      </Link>
+    </section>
+  );
 };
 
 export default ErrorPage;
--- src/Components/FeaturedFood/FeaturedFoodCard.jsx
+++ src/Components/FeaturedFood/FeaturedFoodCard.jsx
@@ -1,55 +1,80 @@
 import React, { useEffect } from "react";
 import { Link } from "react-router-dom";
-import 'aos/dist/aos.css';
+import "aos/dist/aos.css";
 import Aos from "aos";
-
+import { IoLocationSharp } from "react-icons/io5";
+import { FcExpired } from "react-icons/fc";
 
 const FeaturedFoodCard = ({ foodData }) => {
+  useEffect(() => {
+    Aos.init({
+      duration: 1000,
+      easing: "ease-in-out",
+      once: true,
+    });
+  }, []);
 
-    useEffect(() => {
-        Aos.init({
-            duration: 1000,
-            easing: 'ease-in-out',
-            once: true,
-        });
-    }, []);
+  const {
+    _id,
+    foodName,
+    foodImage,
+    foodQuantity,
+    pickupLocation,
+    expiredDateTime,
+    additionalNotes,
+    foodStatus,
+    donatorName,
+  } = foodData;
+  return (
+    <div
+      data-aos="flip-left"
+      className=" w-80 lg:w-96 bg-white rounded-lg shadow-md overflow-hidden border border-gray-200"
+    >
+      <img
+        src={foodImage}
+        alt={foodName}
+        className="w-full h-48 object-cover"
+      />
+      <div className="p-5">
+        <h3 className="text-2xl font-bold text-gray-800">{foodName}</h3>
 
-    const { _id, foodName, foodImage, foodQuantity, pickupLocation, expiredDateTime, additionalNotes, foodStatus, donatorName } = foodData
-    return (
-        <div data-aos="flip-left" className="max-w-sm bg-white rounded-lg shadow-md overflow-hidden border border-gray-200">
-            <img
-                src={foodImage}
-                alt={foodName}
-                className="w-full h-48 object-cover"
-            />
-            <div className="p-5">
-                <h3 className="text-2xl font-bold text-gray-800">{foodName}</h3>
+        <p className="text-sm my-1 font-semibold bg-green-200 w-1/2 text-center rounded-xl">
+          {foodStatus}
+        </p>
 
-                <p className='text-sm my-1 font-semibold bg-green-200 w-1/2 text-center rounded-xl'>Status: {foodStatus}</p>
-
-                <p className="text-sm text-gray-600 mb-2">
-                    <strong>Quantity:</strong> {foodQuantity}
-                </p>
-                <p className="text-sm text-gray-600 mb-2">
-                    <strong>Pickup Location:</strong> {pickupLocation}
-                </p>
-                <p className="text-sm text-gray-600 mb-2">
-                    <strong>Expires On:</strong> {new Date(expiredDateTime).toLocaleString()}
-                </p>
-                <p className="text-sm text-gray-600 mb-4">
-                    <strong>Notes:</strong> {additionalNotes}
-                </p>
-                <p className="text-sm text-gray-600 mb-4">
-                    <strong>Donator:</strong> {donatorName || "Anonymous"}
-                </p>
-                <p
-                    className={`text-sm font-semibold `}>Status: {foodStatus}</p>
-                <Link to={`/food-details/${_id}`} className="mt-4 btn bg-button hover:bg-btnHover text-black px-4 py-2 rounded-md transition">
-                    View Details
-                </Link>
-            </div>
-        </div>
-    );
+        <p className="text-sm text-gray-600 mb-2">
+          <strong>Quantity:</strong> {foodQuantity}
+        </p>
+        <p className="text-sm text-gray-600 mb-2 flex items-center gap-2">
+          <strong>
+            <span>
+              <IoLocationSharp size={20} />
+            </span>
+          </strong>{" "}
+          {pickupLocation}
+        </p>
+        <p className="text-sm text-gray-600 mb-2 flex items-center gap-2">
+          <strong>
+            <FcExpired size={20} />
+          </strong>{" "}
+          {new Date(expiredDateTime).toLocaleString()}
+        </p>
+        <p className="text-sm text-gray-600 mb-4">
+          <strong>Notes:</strong> {additionalNotes}
+        </p>
+        <p className="text-sm text-gray-600 mb-4">
+          <strong>Donator:</strong> {donatorName || "Anonymous"}
+        </p>
+        <p className={`text-sm font-semibold `}>Status: {foodStatus}</p>
+        <Link
+          to={`/food-details/${_id}`}
+          className="mt-4 btn bg-button hover:bg-btnHover text-black px-4 py-2 rounded-md transition"
+        >
+          View Details
+        </Link>
+      </div>
+    </div>
+  );
 };
 
 export default FeaturedFoodCard;
